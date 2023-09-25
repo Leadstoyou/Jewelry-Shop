976b62b0b218c338ad9b6dbd8b76c2c8d157af64
vu minh tu <vuv909@gmail.com>
2023-09-25 10:00:15 +0700

    root fe

---
+++ Jewelry Shop_Front-End/src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function Homepage() {
+  return (
+    <Container>
+      Navbar
+      Homepage
+      Footer
+    </Container>
+  );
+}
+
+export default Homepage;
