56ecec6f734d7cf58a8b496df119f6da053788a6
QuanBui <quanbui030@gmail.com>
2023-09-29 00:05:40 +0700

    Done Login

--- Jewelry Shop_Front-End/src/components/Logins.jsx
+++ Jewelry Shop_Front-End/src/components/Logins.jsx
@@ -1,34 +1,103 @@
-import React from 'react'
-import '../style/Login.css'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/Login.css";
+import axios from "axios";
+
 const Logins = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (validate()) {
+      try {
+        
+        const response = await axios.get("http://localhost:3001/Account", {
+          email: email,
+          password: password,
+        });
+
+        if (response.status === 200) {
+         
+          console.log("Login successful");
+          navigate("/"); 
+        } else {
+       
+          console.error("Login failed");
+        }
+      } catch (error) {
+        
+        console.error("An error occurred:", error);
+      }
+    }
+  };
+
+  const validate = () => {
     
-    return (
- <div className="containers">
-  <div className="header-page">
-    <h1>Đăng nhập</h1>
-  </div>
-  <form action="">
-    <div className="email">
-      <input required type="text" placeholder='Email' size={30} />
-    </div>
-    
-    <div className="password">
-      <input required type="password" size={30} placeholder='Mật khẩu' />
-    </div>
-    <div className="note">
-    <a  href='http://localhost:5173'><span>Quên mật khẩu</span>  </a>? hoặc
-   <a  href='http://localhost:5173/register'><span>Đăng kí</span>  </a>
-    </div>
-    <div className="action_button">
-      <input type="submit" value={"Đăng Nhập"} className="btn" />
-    </div>
+    const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
 
-  </form>
-</div>
+    if (email === "" || email === null ||!email.includes("@")) {
+      alert("Email is required");
+      return false;
+    }
 
-    )
-}
+    if (password === "" || password === null) {
+      alert("Please enter a password");
+      return false;
+    }
 
+    if (!password.match(passwordRegex)) {
+      alert("Password must contain at least one number and one letter.");
+      return false;
+    }
 
-export default Logins
+    return true;
+  };
+
+  return (
+    <div className="containers">
+      <div className="header-page">
+        <h1>Đăng nhập</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="email">
+          <input
+            required
+            type="text"
+            placeholder="Email"
+            size={30}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="password">
+          <input
+            required
+            type="password"
+            size={30}
+            placeholder="Mật khẩu"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="note">
+          <a href="http://localhost:5173">
+            <span>Quên mật khẩu</span>{" "}
+          </a>
+          ? hoặc
+          <a href="http://localhost:5173/register">
+            <span>Đăng kí</span>{" "}
+          </a>
+        </div>
+        <div className="action_button">
+          <input type="submit" value={"Đăng Nhập"} className="btn" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Logins;
--- Jewelry Shop_Front-End/src/components/Registers.jsx
+++ Jewelry Shop_Front-End/src/components/Registers.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import '../style/Register.css'
 const Registers = () => {
-  const inputStyle = {
-    width: '230px',    // Độ rộng 100%
-    height: '32px',   // Độ cao 55px
-  };
+ 
   return (
 
     <div className="container">
@@ -12,20 +9,18 @@ const Registers = () => {
         <h1>Đăng ký</h1>
       </div>
       <form action="">
-        <div className="first_name">
-          <input required type="text" placeholder='Tên' size={30} />
+        <div className="user_name">
+          <input required type="text" placeholder='Họ và Tên' size={30} />
         </div>
-        <div className="last_name">
-          <input required type="text" placeholder='Họ' size={30} />
-        </div>
+        
         <div className="gender">
           <input type="radio" id="radio1" value={1} size={30} />
           <label htmlFor="radio1">Nam</label>
           <input type="radio" id="radio2" value={2} size={30} />
           <label htmlFor="radio2">Nữ</label>
         </div>
-        <div className="birthday">
-          <input type="date" style={inputStyle} placeholder='Ngày sinh nhật' />
+        <div className="age">
+          <input type="text"  placeholder='Tuổi'  size={30}/>
         </div>
         <div className="email">
           <input type="email" size={30} placeholder='Email' />
