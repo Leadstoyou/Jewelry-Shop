017e7c89d2aae8b312c778b4050cdc54cc8067e7
vu minh tu <vuv909@gmail.com>
2023-10-01 22:38:23 +0700

    push all

--- Jewelry Shop_Front-End/src/App.jsx
+++ Jewelry Shop_Front-End/src/App.jsx
@@ -3,24 +3,20 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import styled from 'styled-components'
-import CartPage from './pages/Cart';
-
+import Dashboard from './pages/Dashboard';
 
 const Container = styled.div``
+
 function App() {
   return <Container>
      <BrowserRouter>
      <Routes>
+
       <Route path="/" element={<Homepage />} />
-      <Route path="/cart" element={<CartPage />} />
+      <Route path="/dashboard"  element={<Dashboard />}/>
      </Routes>
      </BrowserRouter>
   </Container>;
 }
 
-
-
-
 export default App;
-
-
