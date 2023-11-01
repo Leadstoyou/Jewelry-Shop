6187825ddc37ace2663396fdeef270ecd33240dc
HOP NGUYEN <hopnvhe164017@fpt.edu.vn>
2023-11-01 09:44:41 +0700

    commit đầy đủ

--- Jewelry Shop_Back-End/routers/orderRouter.js
+++ Jewelry Shop_Back-End/routers/orderRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {orderController} from "../controllers/indexController.js";
 import routeUnknown from "../middleware/routeMiddleware.js";
+import { checkUser } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-router.post('/checkouts', orderController.createOrder);
-router.get('/view', orderController.getOrder);
+router.post('/checkouts', checkUser, orderController.createOrder);
+router.get('/view', checkUser, orderController.getOrder);
 router.get('/getAll',orderController.getAllOrder);
 router.patch('/update/:orderId',orderController.updateOrderStatus);
 router.use(routeUnknown);
